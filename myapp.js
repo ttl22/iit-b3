1266f1baa11b5422df129b2a53b5b54338115cae
badtux <badtux@users.noreply.github.com>
2023-07-01 13:11:20 +0530

    for demo

--- myapp.js
+++ myapp.js
@@ -1,8 +1,8 @@
 const http = require('http');
 const url = require('url');
+const port = 5000;
 
 const hostname = '127.0.0.1';
-const port = 5000;
 
 const server = http.createServer((req, res) => {
   const queryObject = url.parse(req.url,true).query;
@@ -14,4 +14,5 @@ const server = http.createServer((req, res) => {
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
+  console.log('ID123 Hello Name');
 });
