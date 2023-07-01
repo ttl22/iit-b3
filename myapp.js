253b0dd87d15d823705be331bfa1f9f27b31832f
badtux <badtux@users.noreply.github.com>
2023-07-01 12:12:43 +0530

    Create myapp.js

---
+++ myapp.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const url = require('url');
+
+const hostname = '127.0.0.1';
+const port = 5000;
+
+const server = http.createServer((req, res) => {
+  const queryObject = url.parse(req.url,true).query;
+  let result = parseInt(queryObject.a) + parseInt(queryObject.b);
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end(`Result: ${result}\n`);
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
