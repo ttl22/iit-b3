a98504c76a78015173246b0132080fff8d9a83ea
induraaa <129575423+induraaa@users.noreply.github.com>
2023-07-07 22:13:38 +0530

    indura added cmd line to print 'Hello ID007'

--- myapp.js
+++ myapp.js
@@ -15,4 +15,5 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
   console.log('ID123 Hello Name');
+  console.log('Hello ID007');
 });
