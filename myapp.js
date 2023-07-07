c6d42bc525e43d23f73c85de101dbb315fa1d677
induraaa <indurad2@gmail.com>
2023-07-07 22:15:09 +0530

    Update myapp.js

--- myapp.js
+++ myapp.js
@@ -1,18 +1,19 @@
-const http = require('http');
-const url = require('url');
+const http = require("http");
+const url = require("url");
 const port = 5000;
 
-const hostname = '127.0.0.1';
+const hostname = "127.0.0.1";
 
 const server = http.createServer((req, res) => {
-  const queryObject = url.parse(req.url,true).query;
+  const queryObject = url.parse(req.url, true).query;
   let result = parseInt(queryObject.a) + parseInt(queryObject.b);
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader("Content-Type", "text/plain");
   res.end(`Result: ${result}\n`);
 });
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
-  console.log('ID123 Hello Name');
+  console.log("ID123 Hello Name");
+  console.log("Hello ID007");
 });
